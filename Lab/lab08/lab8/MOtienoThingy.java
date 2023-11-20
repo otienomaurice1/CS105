84aec3c7cc29ddbae3767406b752f90e9bca1713
Maurice Otieno <otienom@ad.lafayette.edu>
2023-11-19 22:45:57 -0500

    added finished lab 8

--- Lab/lab08/lab8/MOtienoThingy.java
+++ Lab/lab08/lab8/MOtienoThingy.java
@@ -1,71 +1,107 @@
+//This class is the template for drawing sea worms 
+//it contains two public methods 
+
 import processing.core.*;
 
 class MOtienoThingy {
+  //declare a variable for the Appelet type
   PApplet P;
-  int fillcolor;                   // declare and initialize initial fill color
-  int strokecolor ;               // declare and initialize initial stroke color
-  int transparency ;                // declare and initialize transparency
-  float widthOfMandala ;          // declare and initialize the diameter of the star
-  int  numberOfPoints;              // number of points in each star of the mandala
-  float outer_radius ;               // outermost radius of the star
-  float inner_radius ;           // innermost radius of the star 
+  // declare variable for transparency
+  int transparency ;   
+  //  declare the size of the worm
+  float size_of_worm ;         
+  // declare the number of spikes in a cross section of the worm
+  int  number_of_spikes;      
+  // the outer size of the worm
+  float outer_radius ;    
+  // the inner size of the worm
+  float inner_radius ;    
+  //declare variable for the position of the worm
   float x;
   float y; 
-  int numberOfSpikes = 5;
+  //declare length of worm
+  int length_of_worm = 10;
 
+  // declare the default constructor
   MOtienoThingy(PApplet P) {
     this.P = P;
-    fillcolor = 100;                   // declare and initialize initial fill color
-    strokecolor = 255;               // declare and initialize initial stroke color
-    transparency = 200;                // declare and initialize transparency
-    widthOfMandala = P.width/6;          // declare and initialize the diameter of the star
-    numberOfPoints= 32;              // number of points in each star of the mandala
-    outer_radius = widthOfMandala/2;               // outermost radius of the star
-    inner_radius = (float)(outer_radius *0.7);           // innermost radius of the star
-    x = P.width/2;
-    y =P.height/2;
+    // initialize transparency
+    transparency = 100;       
+    // initialize the size of the worm
+    size_of_worm = P.width/12;
+    // declare the number of spikes in a cross section of the worm
+    number_of_spikes= 32;           
+    // the outer size of the worm
+    outer_radius = size_of_worm/2;              
+    inner_radius = (float)(outer_radius *0.4);         
+    //initialize  the position of the worm
+    x = P.width/8;
+    y =P.height/(2);
   }
-  //MOtienoThingy(){}
 
+  //declare a secondary constructor
+  MOtienoThingy(PApplet P, float xposition, float yposition) {
+    this.P = P;
+    x = xposition;
+    y = yposition;
+  }
+  // declare a method to draw the cross-section of the worm
 
-  void star() { // star function header
+  private void worm(float xCoord, float yCoord) { // star function header
 
     float inR =inner_radius; 
     float outR = outer_radius; 
-    int nPoint = numberOfPoints;
+    int nPoint = number_of_spikes;
     float pie = P.PI;
-    float rotateAngle = (2*pie)/nPoint;            // declare and initialize the angle for which you want to locate upper vertices
-    float midAngle = rotateAngle/2;               //declare and initialize the offset angle for which you want to locate lower vertices
+    // declare and initialize the angle for which you want to locate upper vertices
+    float rotateAngle = (2*pie)/nPoint; 
+    //declare and initialize the offset angle for which you want to locate lower vertices
+    float midAngle = rotateAngle/2;               
 
     P.beginShape();
+
     for (float currentAngle = 0; currentAngle <= (2*pie); currentAngle += rotateAngle) { // declare the current angle 
-      float xpoint1 = x+ P.cos(currentAngle)*outR;            // x coordinate of first vertex
-      float ypoint1 =y +P.sin(currentAngle)*outR;            // x coordinate of first vertex
-      float xpoint2 = x+ P.cos(midAngle+currentAngle)*inR;    // x coordinate of second vertex
-      float ypoint2 = y+P.sin (midAngle+currentAngle)*inR;    // y coordinate of second vertex
+      float xpoint1 = xCoord+ P.cos(currentAngle)*outR;                                  // x coordinate of first vertex
+      float ypoint1 =yCoord +P.sin(currentAngle)*outR;                                   // x coordinate of first vertex
+      float xpoint2 = xCoord+ P.cos(midAngle+currentAngle)*inR;                         // x coordinate of second vertex
+      float ypoint2 = yCoord+P.sin (midAngle+currentAngle)*inR;                          // y coordinate of second vertex
 
 
-      P.fill(fillcolor, transparency);   // set the color 
-      P.stroke(strokecolor);
+      P.fill(100, 255, 100, transparency);   // set the color 
+      P.stroke(100, 55, 100);
 
       // draw  an edge of the star
       P.vertex(xpoint1, ypoint1);
+
       P.vertex(xpoint2, ypoint2);
     }
     P.endShape();
+    P.fill(0);
+    P.circle(x, y, 25);
+    P.fill(22, 0, 0);
+    P.circle(x, y, 10);
   }
 
   //method to move creature
-  void move() {
+  public void move() {
+    if (x== 0) {
+      x++;
+    }
+    if (x<P.width-1) {
+      x+=1;
+    } else if (x >= P.width-1) {
+      x-=1;
+    }
   }
-
-
-
-  void display() {
-    for (int i =0; i<= numberOfSpikes; i++) {
-      star();
-      x+=10;
-      y-=10;
+  //method to display the worm
+  public void display() {
+    float yCoord, xCoord;
+    yCoord = y;
+    xCoord = x;
+    for (int i =0; i<= length_of_worm; i++) {
+      //rotate each worm cross-section before drawing the next
+      P.rotate(P.radians(1));
+      worm(xCoord, yCoord);
     }
   }
 }
--- Lab/lab08/lab8/lab8.pde
+++ Lab/lab08/lab8/lab8.pde
@@ -1,11 +1,32 @@
-MOtienoThingy creature1 ;
+/*
+Project: Sea worm
+Author: Maurice Otieno
+Description: ------------------------------------------------------------------
+             This project entails a sketch to draw a ugly sea worm 
+             the sea worm moves by drifting across the dark,deep waters of the sea
+             the seaworm is dark in color with purple spikes and green across its body 
+             to make it more scary
+             its mouth is a circle and it has no eyes
+             -----------------------------------------------------------------------
+             Date:11/19/2023
+*/
 
-void setup(){
-  size(1000,1000);
-  background(0);
 
-creature1 = new MOtienoThingy(this);
+//create instances of the MotienoThingy class
+MOtienoThingy seaworm1 ;
+MOtienoThingy seaworm2;
+void setup() {
+  //set the size of the canvas to 800 by 600
+  size(800, 600);
+//create the seaworm
+  seaworm1 = new MOtienoThingy(this);
+ //seaworm2 = new MOtienoThingy(this, 400,500);
+
 }
-void draw(){
-  creature1.display();
+void draw() {
+  background(2);
+ seaworm1.display();
+  seaworm1.move();
+ // seaworm2.move();
+ // seaworm2.display();
 }
