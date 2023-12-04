5009db662d5b5383c934a3d40db4a3d331cfe16d
otienomaurice1 <134706372+otienomaurice1@users.noreply.github.com>
2023-12-03 20:10:29 -0500

    Update hardwareEngineer.java

--- Lab/Engineers_conversation_draft/hardwareEngineer.java
+++ Lab/Engineers_conversation_draft/hardwareEngineer.java
@@ -1,1 +1,34 @@
+public class hardwareEngineer {
+  //declare the default constructor 
+  hardwareEngineer() {
+  }
+  
+  //declare a seconadry constructor
+  hardwareEngineer(int x) {
+  }
+  //declare the display method
+  //this method displays the physical features of the engineer 
+  public void display() {
+  }
+
+  public void speak( String engineer1_words) {
+  }
+  
+  //the method arrayWords is the method that contains the hardcoded array of words to be spoken
+//this method contains  two local variables i.e speech and word
+//the speech variable contains the array of words
+//the word variable is the variable for the specific words the software engineer should speak
+  public String arrayWords() {
+    //declare an array of words to be spoken by the software engineer
+    String [] speech = {" HEr", 
+      "Hers"};
+    //declare variable to hold the specific word we want to access
+    String word = " ";
+    //LOOP THROUGH ARRAY
+    for (int i = 0; i< speech.length; i++) {
+      word = speech[i];
+    }
+    return word;
+  }
+}
 
