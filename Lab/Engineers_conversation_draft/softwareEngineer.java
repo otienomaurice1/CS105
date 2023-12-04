62ccdb999279a89fa2e163c191259506c46c3473
otienomaurice1 <134706372+otienomaurice1@users.noreply.github.com>
2023-12-03 20:49:05 -0500

    Update softwareEngineer.java

--- Lab/Engineers_conversation_draft/softwareEngineer.java
+++ Lab/Engineers_conversation_draft/softwareEngineer.java
@@ -1,4 +1,42 @@
-public class softWareEngineer {
+public class softwareEngineer extends Engineers_conversation{
+
+  //declare the fields for the x and y location of the software engineer
+  int x;
+  int y;
+  //declare the default constructor 
+  softwareEngineer() {
+  }
+
+  //declare a seconadry constructor
+  //this constructor gets the x and y position of the software engineer  
+  //depending on the pane in which we want to draw him
+  softwareEngineer(int x, int y) {
+  }
+  //declare the display method
+  //this method displays the physical features of the engineer 
   public void display() {
   }
+// this methd displays the text to be displayed when the mouse hovers over the software engineer
+//it receives a string from the arrayWords method and displays it
+  public void speak( String engineer1_words) {
+  }
+
+  //the method arrayWords is the method that contains the hardcoded array of words to be spoken
+  //this method contains  two local variables i.e speech and word
+  //the speech variable contains the array of words
+  //the word variable is the variable for the specific words the software engineer should speak
+ //this method access the which_Pane() method in Engineer class
+  public String arrayWords() {
+   
+    //declare an array of words to be spoken by the software engineer
+    String [] speech = {" HEr", "Hers"};
+    
+    //declare variable to hold the specific word we want to access
+    //we will get hte specific word we want to access by looping through the array
+    //we wiill select a string depending on the vaalue returned by whichpane() method
+    String word = " ";
+    
+    return word;
+  }
+  
 }
